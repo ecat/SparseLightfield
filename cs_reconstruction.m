ec77b491a3655d881e6895ee09a2aaaa1deab28b
Regina <Anqi@DN525ahp.SUNet>
2017-02-26 15:17:46 -0800

    I tried to compute AreconFourierBasis

--- cs_reconstruction.m
+++ cs_reconstruction.m
@@ -6,16 +6,18 @@ function cs_reconstruction(lightFieldImage)
     M = 8;
 
     % generate masks
-    % uniform masks, size (angularViewResizeFactor x angularViewResizeFactor x M)
-    masks = zeros(3, 3); %% Change this
+       masks = rand(angularViewResizeFactor,angularViewResizeFactor,M);
+  
+%     % uniform masks, size (angularViewResizeFactor x angularViewResizeFactor x M)
+%     masks = zeros(3, 3); %% Change this
 
     % apply masks to the raw data
-    % Y = masks * raw data;  %% Change this
+      maskedImage = masks .* lightFieldImage ;  
 
     % solve the reconstruction problem
-    sigma = 0.1;
+      sigma = 0.1;
     % B = measurements vectorized
-
+     
     % perform once for each colour channel
     for c = 1 :3
         %spg_bpdn(AReconFourierBasis, B, sigma)  %% Change this        
@@ -23,19 +25,24 @@ function cs_reconstruction(lightFieldImage)
 
     % calculate MSE between recovered image and original image
     % lightFieldImage.lightField   %% Change this
-
+  
     function v  = AReconFourierBasis( w, mode )
     %ARECONFOURIERBASIS 
-    %    returns  v = A *w  if mode == 1;
-    %             v = A'*w  if mode == 2. 
-
-        masks
+        if mode ==1
+        return   
+        elseif mode == 2
+        return  v = sum(repmat(reshape(w, [1 1 M]), ...
+            [angularViewResizeFactor, angularViewResizeFactor, 1]).*masks, 3);
+        end
+        
     
         % w is in the sparse basis
         if(mode == 1)
             % apply ifft2 in the u and v axes
-
+            temp1 = repmat(ifft2(w), [1 1 M]);
             % apply masks
+            v = squeeze(sum(sum(masks .* temp1,1),2));
+            
         elseif(mode == 2)
             % apply masks adjoint operation, creates linear combination of
             % masks
